52cf38830b44c4d79adfa86c8575a3c36f8358ea
A_loxxha <charleskush77@gmail.com>
2026-09-25 02:08:47 +0300

    index.js first commit

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
-//! Start by creating the variables for the data recorded
-//* Then work on the conversion of the temperature from Celsius to Fahrenheit (or viceversa)
+const day1ConvertedCelsius = (day1TempF - 32) * 5 / 9;
+const day2ConvertedCelsius = day2TempC;
+const day3ConvertedCelsius = (day3TempF - 32) * 5 / 9;
+const day4ConvertedCelsius = day4TempC;
+const day5ConvertedCelsius = (day5TempF - 32) * 5 / 9;
+const day6ConvertedCelsius = day6TempC;
+const day7ConvertedCelsius = (day7TempF - 32) * 5 / 9;
+const day8ConvertedCelsius = day8TempC;
+const day9ConvertedCelsius = (day9TempF - 32) * 5 / 9;
+const day10ConvertedCelsius = day10TempC;
+const day11ConvertedCelsius = (day11TempF - 32) * 5 / 9;
+const day12ConvertedCelsius = day12TempC;
+const day13ConvertedCelsius = (day13TempF - 32) * 5 / 9;
+const day14ConvertedCelsius = day14TempC;
+const day15ConvertedCelsius = (day15TempF - 32) * 5 / 9;
+const day16ConvertedCelsius = day16TempC;
+const day17ConvertedCelsius = (day17TempF - 32) * 5 / 9;
+const day18ConvertedCelsius = day18TempC;
+const day19ConvertedCelsius = (day19TempF - 32) * 5 / 9;
+const day20ConvertedCelsius = day20TempC;
+const day21ConvertedCelsius = (day21TempF - 32) * 5 / 9;
+const day22ConvertedCelsius = day22TempC;
+const day23ConvertedCelsius = (day23TempF - 32) * 5 / 9;
+const day24ConvertedCelsius = day24TempC;
+const day25ConvertedCelsius = (day25TempF - 32) * 5 / 9;
+const day26ConvertedCelsius = day26TempC;
+const day27ConvertedCelsius = (day27TempF - 32) * 5 / 9;
+const day28ConvertedCelsius = day28TempC;
+const day29ConvertedCelsius = (day29TempF - 32) * 5 / 9;
+const day30ConvertedCelsius = day30TempC;
 
+const tot_temperature_in_celsius =  day1ConvertedCelsius + day2ConvertedCelsius + day3ConvertedCelsius + day4ConvertedCelsius + 
+  day5ConvertedCelsius + day6ConvertedCelsius + day7ConvertedCelsius + day8ConvertedCelsius + 
+  day9ConvertedCelsius + day10ConvertedCelsius + day11ConvertedCelsius + day12ConvertedCelsius + 
+  day13ConvertedCelsius + day14ConvertedCelsius + day15ConvertedCelsius + day16ConvertedCelsius + 
+  day17ConvertedCelsius + day18ConvertedCelsius + day19ConvertedCelsius + day20ConvertedCelsius + 
+  day21ConvertedCelsius + day22ConvertedCelsius + day23ConvertedCelsius + day24ConvertedCelsius + 
+  day25ConvertedCelsius + day26ConvertedCelsius + day27ConvertedCelsius + day28ConvertedCelsius + 
+  day29ConvertedCelsius + day30ConvertedCelsius;
 
-//! Start the calculation of the total temperatures
-//* Then apply the conversion to calculate the total in the other unit of measurement
-//* Call the variables: tot_temperature_in_fahrenheit and tot_temperature_in_celsius
+  const avg_temperature_in_celsius = tot_temperature_in_celsius /30;
 
-//! Start the calculation of the average temperatures
-//* Call the variables: avg_temperature_in_fahrenheit and avg_temperature_in_celsius
-
-//! Console.log the results for your own inspection if you'd like
-
-//! After creating the four variables mentioned above, uncomment the following lines
-//* This way you can export them to the test file, this is essential for the tests to work
-
-module.exports = {
-    // tot_temperature_in_fahrenheit,
-    // tot_temperature_in_celsius,
-    // avg_temperature_in_fahrenheit,
-    // avg_temperature_in_celsius
-};
+  console.log(avg_temperature_in_celsius)

99d2144f58da262507980bec6178853ac6910aec
A_loxxha <charleskush77@gmail.com>
2026-09-25 15:09:46 +0300

    expressions Js

--- index.js
+++ index.js
@@ -1,43 +1,73 @@
-const day1ConvertedCelsius = (day1TempF - 32) * 5 / 9;
-const day2ConvertedCelsius = day2TempC;
-const day3ConvertedCelsius = (day3TempF - 32) * 5 / 9;
-const day4ConvertedCelsius = day4TempC;
-const day5ConvertedCelsius = (day5TempF - 32) * 5 / 9;
-const day6ConvertedCelsius = day6TempC;
-const day7ConvertedCelsius = (day7TempF - 32) * 5 / 9;
-const day8ConvertedCelsius = day8TempC;
-const day9ConvertedCelsius = (day9TempF - 32) * 5 / 9;
-const day10ConvertedCelsius = day10TempC;
-const day11ConvertedCelsius = (day11TempF - 32) * 5 / 9;
-const day12ConvertedCelsius = day12TempC;
-const day13ConvertedCelsius = (day13TempF - 32) * 5 / 9;
-const day14ConvertedCelsius = day14TempC;
-const day15ConvertedCelsius = (day15TempF - 32) * 5 / 9;
-const day16ConvertedCelsius = day16TempC;
-const day17ConvertedCelsius = (day17TempF - 32) * 5 / 9;
-const day18ConvertedCelsius = day18TempC;
-const day19ConvertedCelsius = (day19TempF - 32) * 5 / 9;
-const day20ConvertedCelsius = day20TempC;
-const day21ConvertedCelsius = (day21TempF - 32) * 5 / 9;
-const day22ConvertedCelsius = day22TempC;
-const day23ConvertedCelsius = (day23TempF - 32) * 5 / 9;
-const day24ConvertedCelsius = day24TempC;
-const day25ConvertedCelsius = (day25TempF - 32) * 5 / 9;
-const day26ConvertedCelsius = day26TempC;
-const day27ConvertedCelsius = (day27TempF - 32) * 5 / 9;
-const day28ConvertedCelsius = day28TempC;
-const day29ConvertedCelsius = (day29TempF - 32) * 5 / 9;
-const day30ConvertedCelsius = day30TempC;
+//Celsius
 
-const tot_temperature_in_celsius =  day1ConvertedCelsius + day2ConvertedCelsius + day3ConvertedCelsius + day4ConvertedCelsius + 
-  day5ConvertedCelsius + day6ConvertedCelsius + day7ConvertedCelsius + day8ConvertedCelsius + 
-  day9ConvertedCelsius + day10ConvertedCelsius + day11ConvertedCelsius + day12ConvertedCelsius + 
-  day13ConvertedCelsius + day14ConvertedCelsius + day15ConvertedCelsius + day16ConvertedCelsius + 
-  day17ConvertedCelsius + day18ConvertedCelsius + day19ConvertedCelsius + day20ConvertedCelsius + 
-  day21ConvertedCelsius + day22ConvertedCelsius + day23ConvertedCelsius + day24ConvertedCelsius + 
-  day25ConvertedCelsius + day26ConvertedCelsius + day27ConvertedCelsius + day28ConvertedCelsius + 
-  day29ConvertedCelsius + day30ConvertedCelsius;
+const day1TempC = (32 - 32) * 5 / 9;
+const day2TempC = 25
+const day3TempC = (70 - 32) * 5 / 9
+const day4TempC = 18
+const day5TempC = (80 - 32) * 5 / 9
+const day6TempC = 15
+const day7TempC = (72 - 32) * 5 / 9
+const day8TempC = 28
+const day9TempC = (68 - 32) * 5 / 9
+const day10TempC = 20
+const day11TempC = (75 - 32) * 5 / 9
+const day12TempC = 23
+const day13TempC = (82 - 32) * 5 / 9
+const day14TempC = 30
+const day15TempC = (65 - 32) * 5 / 9
+const day16TempC = 22
+const day17TempC = (77 - 32) * 5 / 9
+const day18TempC = 26
+const day19TempC = (78 - 32) * 5 / 9
+const day20TempC = 24
+const day21TempC = (73 - 32) * 5 / 9
+const day22TempC = 21
+const day23TempC = (79 - 32) * 5 / 9
+const day24TempC = 27
+const day25TempC = (71 - 32) * 5 / 9
+const day26TempC = 19
+const day27TempC = (74 - 32) * 5 / 9
+const day28TempC = 17
+const day29TempC = (76 - 32) * 5 / 9
+const day30TempC = 29
 
-  const avg_temperature_in_celsius = tot_temperature_in_celsius /30;
+//Fahrenheit
+const day1TempF = 32
+const day2TempF = (25 * 9 / 5) + 32
+const day3TempF = 70
+const day4TempF = (18 * 9 / 5) + 32
+const day5TempF = 80
+const day6TempF = (15 * 9 / 5) + 32
+const day7TempF = 72
+const day8TempF = (28 * 9 / 5) + 32
+const day9TempF = 68
+const day10TempF = (20 * 9 / 5) + 32
+const day11TempF = 75
+const day12TempF = (23 * 9 / 5) + 32
+const day13TempF = 82
+const day14TempF = (30 * 9 / 5) + 32
+const day15TempF = 65
+const day16TempF = (22 * 9 / 5) + 32
+const day17TempF = 77
+const day18TempF = (26 * 9 / 5) + 32
+const day19TempF = 78
+const day20TempF = (24 * 9 / 5) + 32
+const day21TempF = 73
+const day22TempF = (21 * 9 / 5) + 32
+const day23TempF = 79
+const day24TempF = (27 * 9 / 5) + 32
+const day25TempF = 71
+const day26TempF = (19 * 9 / 5) + 32
+const day27TempF = 74
+const day28TempF = (17 * 9 / 5) + 32
+const day29TempF = 76
+const day30TempF = (29 * 9 / 5) + 32
 
-  console.log(avg_temperature_in_celsius) 
+const tot_temperature_in_celsius = (day1TempC + day2TempC + day3TempC + day4TempC + day5TempC + day6TempC + day7TempC + day8TempC + day9TempC + day10TempC + day11TempC + day12TempC + day13TempC + day14TempC + day15TempC + day16TempC + day17TempC + day18TempC + day19TempC + day20TempC + day21TempC + day22TempC + day23TempC + day24TempC + day25TempC + day26TempC + day27TempC + day28TempC + day29TempC + day30TempC);
+
+const tot_temperature_in_fahrenheit = (day1TempF + day2TempF + day3TempF + day4TempF + day5TempF + day6TempF + day7TempF + day8TempF + day9TempF + day10TempF + day11TempF + day12TempF + day13TempF + day14TempF + day15TempF + day16TempF + day17TempF + day18TempF + day19TempF + day20TempF + day21TempF + day22TempF + day23TempF + day24TempF + day25TempF + day26TempF + day27TempF + day28TempF + day29TempF + day30TempF);
+ console.log(tot_temperature_in_celsius);
+
+const avg_temperature_in_fahrenheit = (tot_temperature_in_fahrenheit / 30);
+
+const avg_temperature_in_celsius = (tot_temperature_in_celsius / 30);
